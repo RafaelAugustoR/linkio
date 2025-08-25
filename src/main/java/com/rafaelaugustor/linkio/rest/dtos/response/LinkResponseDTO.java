dd716c5b751e2b79ec05dd193872184d48fd1724
RafaelAugustoR <rafaelaugustodev@gmail.com>
2025-08-24 21:09:44 -0300

    feat: add LinkResponseDTO for handling response data

---
+++ src/main/java/com/rafaelaugustor/linkio/rest/dtos/response/LinkResponseDTO.java
@@ -0,0 +1,12 @@
+package com.rafaelaugustor.linkio.rest.dtos.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LinkResponseDTO {
+    private String shortUrl;
+}
